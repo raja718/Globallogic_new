28ca4366d7e96d9bada78159ac9bfc60f6238c21
Rajasekar A <rajasekar.anbalagan@globallogic.com>
2022-04-13 23:21:31 +0530

    code updated

--- src/test/java/demo/LoginApp_Test.java
+++ src/test/java/demo/LoginApp_Test.java
@@ -17,27 +17,22 @@ public class LoginApp_Test {
 	public void initial() {
 
 		WebDriverManager.chromedriver().setup();
-ChromeOptions options = new ChromeOptions();
-options.setHeadless(true);
+		ChromeOptions options = new ChromeOptions();
+		options.setHeadless(true);
 
 		driver = new ChromeDriver(options);
 		driver.get("http://demowebshop.tricentis.com/login");
 		driver.manage().window().maximize();
 	}
-	
 	@Test
 	private void test1() throws InterruptedException {
-		driver.manage().window().maximize();
-		driver.findElement(By.linkText("Log in")).click();
-		driver.findElement(By.id("Email")).sendKeys("manz@mass.com");
-		driver.findElement(By.id("Password")).sendKeys("mass123");
-		driver.findElement(By.linkText("Books")).click();
-		driver.findElement(By.xpath("(//input[@type='button'])[2]")).click();
-		Thread.sleep(3000);
-		driver.findElement(By.xpath("(//span[@class='cart-label'])[1]")).click();
-		Thread.sleep(3000);
-		driver.findElement(By.linkText("Log out")).click();
+		
+		
+		driver.findElement(By.className("ico-login")).click();
+		
+		
+	
+	}
 	}
 
-	
-}
+
